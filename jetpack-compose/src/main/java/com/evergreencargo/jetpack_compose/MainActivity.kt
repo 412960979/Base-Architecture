f8f96dc01a731db708eca5cf57063f7b99a86f17
wn <412960979@qq.com>
2020-11-19 13:25:51 +0800

    compose

--- jetpack-compose/src/main/java/com/evergreencargo/jetpack_compose/MainActivity.kt
+++ jetpack-compose/src/main/java/com/evergreencargo/jetpack_compose/MainActivity.kt
@@ -2,9 +2,18 @@ package com.evergreencargo.jetpack_compose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
@@ -13,18 +22,52 @@ class MainActivity : AppCompatActivity() {
 //        setContentView(R.layout.activity_main)
 
         setContent {
-            Greeting("Android Greeting")
+            NewsStory()
         }
     }
 }
 
 @Composable
-private fun Greeting(name: String) {
-    Text(name)
+fun NewsStory() {
+    val image = imageResource(R.drawable.header)
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            val imageModifier = Modifier
+                .preferredHeight(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp))
+
+            Image(
+                image, modifier = imageModifier,
+                contentScale = ContentScale.Crop
+            )
+            Spacer(Modifier.preferredHeight(16.dp))
+
+            Text(
+                "A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw.I dont why,byeeeeeeeeeeeeeeee",
+                style = typography.h6,
+                maxLines = 2
+            )
+            Text(
+                "Davenport, California",
+                style = typography.body2,
+                maxLines = 2
+            )
+            Text(
+                "December 2018",
+                style = typography.body2
+            )
+        }
+    }
 }
 
 @Preview
 @Composable
-private fun GreetingPreview() {
-    Greeting("Android Greeting")
+private fun NewsStoryPreview(){
+    NewsStory()
 }
