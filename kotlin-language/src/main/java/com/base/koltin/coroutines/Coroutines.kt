b8e4c9a6d7deb7bb96758e39bb7894cd277d590c
weining <weining412960979@gmail.com>
2020-12-04 01:41:28 +0800

    协程

--- kotlin-language/src/main/java/com/base/koltin/coroutines/Coroutines.kt
+++ kotlin-language/src/main/java/com/base/koltin/coroutines/Coroutines.kt
@@ -1,21 +1,10 @@
 package com.base.koltin.coroutines
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
-fun test1(){
-    GlobalScope.launch {
-        delay((1000L))
-        println("world")
-    }
-    println("hello")
-    Thread.sleep(2000L)
-}
-
-suspend fun testListFlow(){
+suspend fun testListFlow() {
     (1..3).asFlow().collect {
         println(it)
     }
@@ -25,8 +14,46 @@ suspend fun testListFlow(){
     }
 }
 
-suspend fun main(){
-    test1()
 
-//    testListFlow()
+suspend fun main(args: Array<String>) {
+    test2()
+}
+
+suspend fun test2(){
+    println("开始")
+
+    val res0 = GlobalScope.async {
+        var i = 0
+        withContext(Dispatchers.IO){
+            for (index in 0..100){
+                delay(1_00)
+                i++
+                println("${Thread.currentThread()} main 计算中：${i}")
+            }
+
+            i
+        }
+    }
+
+    val res = GlobalScope.async{
+        demo1()
+    }
+    // 两个协程都运行完才执行下面的代码
+    println("最终的值：${res.await()}, ${res0.await()}")
+    println("结束")
+}
+
+suspend fun demo1(): Int {
+    var i = 0
+
+    return withContext(Dispatchers.IO) {
+        for (index in 0..100) {
+            delay(1_00)
+            i++
+            println("${Thread.currentThread()} io 计算中：${i}")
+        }
+
+
+        i
+    }
 }
