1c0c1b6b109ae94b2ef12d52f1ca99667805d802
weining <weining412960979@gmail.com>
2020-12-07 22:56:00 +0800

    自定义view

--- customview/src/main/java/com/base/customview/CustomView.kt
+++ customview/src/main/java/com/base/customview/CustomView.kt
@@ -12,24 +12,32 @@
  */
 package com.base.customview
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
+
 
 class CustomView : View {
     private val mPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val mPath = Path()
 
+    private lateinit var launchBitmap: Bitmap
+    private lateinit var camera: Camera
+    private var degree = 0f
+    @SuppressLint("ObjectAnimatorBinding")
+    private var animator = ObjectAnimator.ofFloat(this, "degree", 0f, 180f)
+
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
+            context,
+            attrs,
+            defStyleAttr
     ) {
         init(context)
     }
@@ -41,6 +49,38 @@ class CustomView : View {
         mPath.addArc(200f, 200f, 400f, 400f, -225f, 225f);
         mPath.arcTo(400f, 200f, 600f, 400f, -180f, 225f, false);
         mPath.lineTo(400f, 542f);
+
+        launchBitmap = BitmapFactory.decodeResource(context.resources, R.mipmap.ic_launch_test)
+        camera = Camera()
+
+        animator.duration = 2500
+        animator.interpolator = LinearInterpolator()
+        animator.repeatCount = ValueAnimator.INFINITE
+        animator.repeatMode = ValueAnimator.REVERSE
+    }
+
+    fun start() {
+        animator.start()
+    }
+
+    fun end() {
+        animator.end()
+    }
+
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        animator.start()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        animator.end()
+    }
+
+    fun setDegree(degree: Float) {
+        this.degree = degree
+        invalidate()
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -48,7 +88,58 @@ class CustomView : View {
 
         canvas.drawColor(Color.parseColor("#FFCCCC"))
 //        canvas.drawCircle(400f, 400f, 180f, mPaint)
+        canvas.drawPath(mPath, mPaint)
 
-        canvas.drawPath(mPath, mPaint);
+//        canvas.save()
+        // 这里有个顺序，先旋转再平移
+        /*canvas.rotate(-30f)
+        canvas.translate(-200f, -100f)*/
+
+//        camera.save()
+//         camera的旋转是以原点（左上角）为轴心的
+//        camera.rotateX(-45f)
+//        camera.rotateY(-30f)
+//        canvas.translate(width / 2.0f, height / 2.0f)
+//        camera.applyToCanvas(canvas)
+//        canvas.translate(-width / 2.0f, -height / 2.0f)
+//        camera.restore()
+//
+//        canvas.drawBitmap(launchBitmap, width / 2.0f - 161, height / 2.0f - 116, null)
+//        canvas.restore()
+
+        val bitmapWidth: Int = launchBitmap.width
+        val bitmapHeight: Int = launchBitmap.height
+        val centerX = width / 2
+        val centerY = height / 2
+        val x = centerX - bitmapWidth / 2.0f
+        val y = centerY - bitmapHeight / 2.0f
+
+        // 第一遍绘制：上半部分
+
+        // 第一遍绘制：上半部分
+        canvas.save()
+        canvas.clipRect(0, 0, width, centerY)
+        canvas.drawBitmap(launchBitmap, x, y, mPaint)
+        canvas.restore()
+
+        // 第二遍绘制：下半部分
+
+        // 第二遍绘制：下半部分
+        canvas.save()
+
+        if (degree < 90) {
+            canvas.clipRect(0, centerY, width, height)
+        } else {
+            canvas.clipRect(0, 0, width, centerY)
+        }
+        camera.save()
+        camera.rotateX(degree)
+        canvas.translate(centerX.toFloat(), centerY.toFloat())
+        camera.applyToCanvas(canvas)
+        canvas.translate((-centerX).toFloat(), (-centerY).toFloat())
+        camera.restore()
+
+        canvas.drawBitmap(launchBitmap, x, y, mPaint)
+        canvas.restore()
     }
 }
--- customview/src/main/java/com/base/customview/MainActivity.kt
+++ customview/src/main/java/com/base/customview/MainActivity.kt
@@ -2,10 +2,33 @@ package com.base.customview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var a: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        customView.setOnClickListener {
+            customView.end()
+            customView.start()
+
+            endAnim()
+        }
+
+        endAnim()
+
+    }
+
+    private fun endAnim() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            customView.end()
+            customView.setDegree(0f)
+        }, 10_000)
     }
 }
