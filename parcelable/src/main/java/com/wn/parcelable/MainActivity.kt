db61ea5dd93b6386cac2c3da536856e06943ec7d
weining <weining412960979@gmail.com>
2021-02-10 04:50:12 +0800

    view binding简单尝试

--- parcelable/src/main/java/com/wn/parcelable/MainActivity.kt
+++ parcelable/src/main/java/com/wn/parcelable/MainActivity.kt
@@ -3,15 +3,22 @@ package com.wn.parcelable
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wn.parcelable.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView
 
         val user = User("wei", "ning", 16)
         Intent().putExtra("user", user)
         val myClass = MyClass(ExternalClass(5))
         Intent().putExtra("myClass", myClass)
+
     }
 }
